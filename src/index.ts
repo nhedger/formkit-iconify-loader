328262269899ad3d5b07d3fccabe03d7ecbc414e
Nicolas Hedger <nicolas@hedger.ch>
2023-08-28 20:51:13 +0200

    feat: support loading multiple icon sets

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,35 @@ import type { FormKitIconLoader } from "@formkit/themes";
 import type { IconifyJSON } from "@iconify/types";
 import { getIconData, iconToSVG, iconToHTML, replaceIDs } from "@iconify/utils";
 
-export const createIconifyLoader = (icons: IconifyJSON): FormKitIconLoader => {
+export const createIconifyLoader = (
+	icons: IconifyJSON,
+	...moreIcons: IconifyJSON[]
+): FormKitIconLoader => {
 	return async (name: string) => {
-		// Retrieve the data for the icon
-		const iconData = getIconData(icons, name);
+		const allIcons = [icons, ...moreIcons];
+		let setName: string | undefined = undefined;
+		let iconName: string | undefined = undefined;
 
-		// If the icon doesn't exist, return undefined
+		// If the name of the icon include a colon, we split it into the set
+		// name and the icon name.
+		if (name.includes(":")) {
+			[setName, iconName] = name.split(":");
+		} else {
+			iconName = name;
+		}
+
+		// Try to find the set to which the icons belongs using the set name.
+		let set: IconifyJSON | undefined;
+		if (setName && iconName) {
+			set = allIcons.find((iconSet) => iconSet.prefix === setName);
+		}
+
+		// Retrieve the data for the icon from the set.
+		// Fallback to the first set if no set was found previously.
+		const iconData = getIconData(set || allIcons[0], iconName);
+
+		// At this point, if no icon data was found, we return undefined
+		// and no icon will be rendered.
 		if (!iconData) {
 			return undefined;
 		}
